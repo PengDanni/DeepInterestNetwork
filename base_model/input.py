4bb7ab2a63802a01f8d3120e0d117f1734819ac0
PengDanni <danni9594@gmail.com>
2020-01-23 21:34:02 +0800

    edit

--- base_model/input.py
+++ base_model/input.py
@@ -31,21 +31,22 @@ class DataInput:
             u.append(t[0])  # user
             i.append(t[2])  # item
             y.append(t[3])  # label
-            sl.append(len(t[1]))  # shopping list / history
+            sl.append(len(t[1]))  # shopping history length
         max_sl = max(sl)
 
-        hist_i = np.zeros([len(ts), max_sl], np.int64)
+        hist_i = np.zeros([len(ts), max_sl], np.int64)  # create a 0 padded numpy matrix of shape(ts_size, max_hist_len)
 
-        k = 0
+        k = 0  # track the sample index
         for t in ts:
             for l in range(len(t[1])):
-                hist_i[k][l] = t[1][l]
+                hist_i[k][l] = t[1][l]  # convert list of history lists into a matrix
             k += 1
 
         return self.i, (u, i, y, hist_i, sl)
 
 
 class DataInputTest:
+    """data input for test after one batch training???"""
     def __init__(self, data, batch_size):
 
         self.batch_size = batch_size
@@ -70,8 +71,8 @@ class DataInputTest:
         u, i, j, sl = [], [], [], []
         for t in ts:
             u.append(t[0])
-            i.append(t[2][0])
-            j.append(t[2][1])
+            i.append(t[2][0])  # positive item
+            j.append(t[2][1])  # negative item
             sl.append(len(t[1]))
         max_sl = max(sl)
 
